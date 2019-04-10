5acff5b6ced40fa68ea586efb26b1477052ae550
daehanlee <dh16931@gmail.com>
2019-04-11 00:37:34 +0900

    flask

---
+++ flask/hello.py
@@ -0,0 +1,34 @@
+# _*_ coding: utf-8 _*_
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author' : 'daehan',
+        'title' : 'post 1',
+        'content' : '1st',
+        'date' : '20190410'
+
+    },
+    {
+        'author': 'fox',
+        'title': 'post 2',
+        'content': '2nd',
+        'date': '20190411'
+
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)  # 앞 posts 는 home.html 에서 오고, 뒤는 위에 post 정보
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")  # title 제공
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
