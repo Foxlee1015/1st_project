039dfdfb0a703ed264b8472cafdca380c3351736
daehanlee <dh16931@gmail.com>
2019-04-11 19:17:27 +0900

    register feedback

--- flask/hello.py
+++ flask/hello.py
@@ -1,8 +1,9 @@
 # _*_ coding: utf-8 _*_
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '50174397'
 posts = [
     {
         'author' : 'Daehan',
@@ -29,6 +30,20 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title="About")  # title 제공
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # home 함수
+    return render_template('register.html', title="Register", form=form)  # title 제공
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Login", form=form)  # title 제공
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
