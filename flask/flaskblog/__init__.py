f41c21149c7cff85ff5f9c3699539ca60f538586
daehanlee <dh16931@gmail.com>
2019-04-13 07:01:11 +0900

    login, account

--- flask/flaskblog/__init__.py
+++ flask/flaskblog/__init__.py
@@ -1,9 +1,15 @@
 # __init__.py -> 파이썬에게 이게 들어있는 폴더(flaskblog)는 패키지다
 from flask import Flask  # render - return 으로 해당 html 나옴 // url_for 템플릿 {{ url_for('home')}} (/home) 아님 // flash - like a popup // redirect(url_for('about') 이동
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '50174397'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 from flaskblog import routes
--- flask/flaskblog/routes.py
+++ flask/flaskblog/routes.py
@@ -1,8 +1,12 @@
-from flask import render_template, url_for, flash, redirect  # render - return 으로 해당 html 나옴 // url_for 템플릿 {{ url_for('home')}} (/home) 아님 // flash - like a popup // redirect(url_for('about') 이동
-from flaskblog import app
+from flask import render_template, url_for, flash, redirect, request  # render - return 으로 해당 html 나옴 // url_for 템플릿 {{ url_for('home')}} (/home) 아님 // flash - like a popup // redirect(url_for('about') 이동
+from flaskblog import app, db
 from flaskblog.forms import RegistrationForm, LoginForm  # 내가 만든 forms.py 정의한 함수(RegistrationForm, LoginForm) import
 from flaskblog.models import User, Post
+from flask_bcrypt import Bcrypt
+from flask_login import login_user, current_user, logout_user, login_required # loginuser- db에 정보 있으면 로그인 확인 메세지/ current_user 로그 된 상태에서 -> 밑에 로그인,레지스터 눌러도 home
 
+
+bcrypt = Bcrypt(app)
 posts = [
     {
         'author' : 'Daehan',
@@ -30,20 +34,40 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()    # forms.py 의 form
     if form.validate_on_submit():   # form 이 정상적으로 제출되면
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home')) # home 함수
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')        # 커맨드에서 로그인 설정
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user) # 비밀번호
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login')) # home 함수
     return render_template('register.html', title="Register", form=form)  # title 제공
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():  # form.validate_on_submit 은 어디서 호출??
-        if form.email.data == 'admin@blog.com' and form.password.data =="password":
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page =request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login unsuccessful. Please check username and password', 'danger')
+            flash('Login unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title="Login", form=form)  # title 제공
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required # 데코
+def account():
+    return render_template('account.html', title="Account")
