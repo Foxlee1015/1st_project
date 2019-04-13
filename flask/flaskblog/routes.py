19a5545a66ffc8488d79f5895886cebf67b448b2
daehanlee <dh16931@gmail.com>
2019-04-13 16:25:14 +0900

    update, profile_pic

--- flask/flaskblog/routes.py
+++ flask/flaskblog/routes.py
@@ -1,12 +1,12 @@
 from flask import render_template, url_for, flash, redirect, request  # render - return 으로 해당 html 나옴 // url_for 템플릿 {{ url_for('home')}} (/home) 아님 // flash - like a popup // redirect(url_for('about') 이동
 from flaskblog import app, db
-from flaskblog.forms import RegistrationForm, LoginForm  # 내가 만든 forms.py 정의한 함수(RegistrationForm, LoginForm) import
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm # 내가 만든 forms.py 정의한 함수(RegistrationForm, LoginForm) import
 from flaskblog.models import User, Post
 from flask_bcrypt import Bcrypt
 from flask_login import login_user, current_user, logout_user, login_required # loginuser- db에 정보 있으면 로그인 확인 메세지/ current_user 로그 된 상태에서 -> 밑에 로그인,레지스터 눌러도 home
 
-
 bcrypt = Bcrypt(app)
+
 posts = [
     {
         'author' : 'Daehan',
@@ -34,7 +34,7 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
+    if current_user.is_authenticated:                  # 로그인한 상태라면
         return redirect(url_for('home'))
     form = RegistrationForm()    # forms.py 의 form
     if form.validate_on_submit():   # form 이 정상적으로 제출되면
@@ -49,7 +49,7 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
+    if current_user.is_authenticated:    # 로그인한 상태라면
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():  # form.validate_on_submit 은 어디서 호출??
@@ -67,7 +67,22 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+@app.route('/account',  methods=['GET', 'POST'])
 @login_required # 데코
 def account():
-    return render_template('account.html', title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('your account has been updated!', "success")
+        return redirect(url_for('account'))
+    elif request.method =='GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/'+ current_user.image_file)
+    return render_template('account.html', title="Account", image_file=image_file, form=form)
+
+@app.route('/patent')
+def patent():
+    return render_template('patent.html', title="Patent")
