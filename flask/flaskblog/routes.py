215a868c2d1c09ef0da5b875cf1e13cd99ee9609
daehanlee <dh16931@gmail.com>
2019-04-13 20:45:41 +0900

    def delete_post

--- flask/flaskblog/routes.py
+++ flask/flaskblog/routes.py
@@ -124,3 +124,14 @@ def update_post(post_id):
         form.content.data = post.content     # form 에 post 를 넣어줌.
     return render_template('create_post.html', title='update post', form=form, legend='Update Post')  # 바로위 form 을 form 으로 정의  # legend 테크,
 
+
+@app.route('/post/<int:post_id>/delete',  methods=['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
