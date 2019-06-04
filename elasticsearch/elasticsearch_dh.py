362d90fda1a71d40d1af44a17beb8beefd086798
daehan <dh16931@gmail.com>
2019-06-04 13:19:57 +0900

    OOP

--- elasticsearch/elasticsearch_dh.py
+++ elasticsearch/elasticsearch_dh.py
@@ -2,8 +2,8 @@ from flask import Flask, request, render_template
 from elasticsearch import Elasticsearch
 from wtforms import Form, validators, SubmitField, TextAreaField, SelectField
 import csv
-
-# Flask 와 연동시 참고
+from data.es_class import Data_handler   # data = 디렉토리, es_class = 파일명(py), Data_handler = 클래스
+from data.es_class import Index_handler
 
 es = Elasticsearch('http://localhost:9200')
 app = Flask(__name__)
@@ -14,21 +14,28 @@ class Search_Form(Form):
     country = SelectField('Country', choices=[('US', 'US'), ('KR', 'KR'), ('JP', 'JP'), ('EP', 'EP')])
     submit = SubmitField('Request Password Reset')
 
-@app.route('/', methods=['GET'])  # 전체 데이
-def index():
+# 현재 저장된 인덱스 리스트 출력
+@app.route('/', methods=['GET'])
+def home():
+    index_data = []
+    for index in es.indices.get_alias("*"):
+        index_data.append(index)
+    return render_template('es_home.html', data=index_data, n=len(index_data))
+
+# 특정 인덱스만 출력
+@app.route('/index/<string:index>/<string:doc_type>', methods=['GET', 'POST'])
+def index(index, doc_type):
     data = []
     for id in range(1,347):    ## 특허 건 수에 맞춰서 수정
-        results = es.get(index='patent', doc_type='patent_test', id=id)
+        results = es.get(index=index, doc_type=doc_type, id=id)
         data.append(results)
     return render_template('es_home.html', data=data, n = len(data))
-
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     form = Search_Form(request.form)
     if request.method == "POST" :
         title, country, abstract = form.title.data, form.country.data, form.abstract.data
-        print(title, country, abstract)
         result = search_data2(country, title, abstract)
         return render_template('es_search.html', results=result, n=len(result), form=form)
     else:
@@ -52,7 +59,6 @@ def search_data1(keyword):
         data = re_hits[i]['_source']
         result.append([data['Country'], data['Application date'], data['Inventor'], data['Title'], data['Abstract'], data['Claim']])
     return result
-
 
 def search_data2(keyword1, keyword2, keyword3):
     body = {
@@ -78,44 +84,11 @@ def search_data2(keyword1, keyword2, keyword3):
         result.append([data['Country'], data['Application date'], data['Inventor'], data['Title'], data['Abstract'], data['Claim']])
     return result
 
-# patent db 에서 넣기
-def post_data(id, csv_data):
-    doc = {}
-    doc['Country'] = csv_data[0]
-    doc['Document_type']= csv_data[1]
-    doc['Title'] = csv_data[2]
-    doc['Abstract'] = csv_data[3]
-    doc['Claim'] = csv_data[4]
-    doc['Number of claims'] = csv_data[5]
-    doc['Application number'] = csv_data[6]
-    doc['Application date'] = csv_data[7]
-    doc['Public number'] = csv_data[8]
-    doc['Public date'] = csv_data[9]
-    doc['Issue number'] = csv_data[10]
-    doc['Issue date'] = csv_data[11]
-    doc['Inventor'] = csv_data[12]
-    res = es.index(index="patent", doc_type='patent_test', id=id, body=doc)
-    print(res['result'])
+#add_data1 = Data_handler("filename", "index", "doc_type")
+#new_data1.upload_data()
 
-# 인덱스 지우기
-def delete_data(index):
-    res = es.indices.delete(index=index, ignore=[400, 404])
-
-# 데이터 csv 파일 읽은 후 es post
-def read_data():
-    f = open('data/data2.csv', 'r', encoding='euc-kr')   # utf-8 에러 발생
-    rdr = csv.reader(f)
-    id = 1 # 기존 데이터 업데이트한다면 1  추가할시 기존 id 마지막 다음으로 설정 (346까지 존재)
-    for line in rdr:
-        if line[0] == "국가코드":   # 첫 행 무시하기
-            pass
-        else:
-            csv_data = line
-            post_data(id, csv_data)   #
-            id += 1
-    f.close()
-
-#read_data()
+#delete_index = Index_handler("index")
+#z.delete()
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
